7ec7ee582169459d237884463ee2895299f7061d
Vuong Le <vle7@uncc.edu>
2017-03-03 00:53:01 +0000

    section 4.8 complete

--- myrottenpotatoes/app/controllers/movies_controller.rb
+++ myrottenpotatoes/app/controllers/movies_controller.rb
@@ -9,6 +9,10 @@ class MoviesController < ApplicationController
     
   end
   
+  def new
+    @movie = Movie.new
+  end
+  
   def create
     params.require(:movie)
     permitted = params[:movie].permit(:title,:rating,:release_date)
@@ -17,8 +21,24 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
   
-  
-  def new
-    @movie = Movie.new
+  def edit
+    @movie = Movie.find params[:id]
   end
+
+  def update
+    @movie = Movie.find params[:id]
+    permitted = params[:movie].permit(:title,:rating,:release_date)
+    @movie.update_attributes!(permitted)  # new way  
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movies_path(@movie)
+  end
+  
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
+  
+
 end
